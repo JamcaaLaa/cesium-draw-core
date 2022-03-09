4b561d34e0147d5fd6b0b02e83a3b9b23d8ce2c5
onsummer <onsummer@foxmail.com>
2022-03-10 00:19:01 +0800

    [update] remove static file and copy after install

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,23 @@ const taskName = process.argv[2]
 
 console.log(`=== Task: ${taskName} will running ===`)
 
+const deleteTargets = [
+  'Assets/**/*',
+  'ThirdParty/**/*',
+  'Widgets/**/*',
+  'Workers/**/*',
+  'Cesium.d.ts',
+  'Cesium.js',
+  'Cesium.js.map'
+]
+
 const staticCopyTask = () => {
-  del('./demo/public/**/*')
+  del(deleteTargets.map((src) => `./demo/public/${src}`))
 
   return gulp
-    .src('./node_modules/cesium/Build/CesiumUnminified/**/*')
+    .src(`./node_modules/cesium/Build/CesiumUnminified/**/*`, {
+      ignore: ['Scene']
+    })
     .pipe(gulp.dest('./demo/public'))
 }
 staticCopyTask.displayName = 'static-copy'
